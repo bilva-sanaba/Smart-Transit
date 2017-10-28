988d203238ce9532339dabb4d499eb50409f952e
jledinh <jledinh@live.unc.edu>
2017-10-28 19:19:15 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,235 @@
+'use strict';
+
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
+ * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
+ * testing instructions are located at http://amzn.to/1LzFrj6
+ *
+ * For additional samples, visit the Alexa Skills Kit Getting Started guide at
+ * http://amzn.to/1LGWsLG
+ */
+
+
+// --------------- Helpers that build all of the responses -----------------------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: 'PlainText',
+            text: output,
+        },
+        card: {
+            type: 'Simple',
+            title: `SessionSpeechlet - ${title}`,
+            content: `SessionSpeechlet - ${output}`,
+        },
+        reprompt: {
+            outputSpeech: {
+                type: 'PlainText',
+                text: repromptText,
+            },
+        },
+        shouldEndSession,
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: '1.0',
+        sessionAttributes,
+        response: speechletResponse,
+    };
+}
+
+
+// --------------- Functions that control the skill's behavior -----------------------
+
+function getWelcomeResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    const sessionAttributes = session.attributes;
+    const cardTitle = 'Welcome';
+    const speechOutput = 'Welcome to the Alexa Smart Transit Skill.';
+    const userId = session.user.userId;
+    sessionAttributes['userId'] = userId;
+    sessionAttributes['consentToken'] = session.user.consentToken;
+    sessionAttributes['deviceId'] = session.device.deviceId;
+    session.attributes = sessionAttributes;
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    const repromptText = 'Sorry I could not understand you. Please repeat what you said.';
+    const shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleSessionEndRequest(callback) {
+    const cardTitle = 'Session Ended';
+    const speechOutput = 'Thank you for using the Alexa Smart Transit Skill. Have a nice day!';
+    // Setting this to true ends the session and exits the skill.
+    const shouldEndSession = true;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+}
+
+/**
+ * Sets the color in the session and prepares the speech to reply to the user.
+ */
+function addCar(intent, session, callback) {
+    const cardTitle = intent.name;
+    const carMake = intent.slots.make;
+    const carModel = intent.slots.model;
+    const carYear = intent.slots.year;
+    let repromptText = '';
+    let sessionAttributes = session.attributes;
+    const shouldEndSession = false;
+    let speechOutput = '';
+    const userId = session.attributes.userId;
+    if (carMake && carModel && carYear) {
+        const currentMake = carMake.value;
+        const currentModel = carModel.value;
+        const currentYear = carYear.value;
+        sessionAttributes['currentMake'] =currentMake;
+        sessionAttributes['currentModel'] = currentModel;
+        sessionAttributes['currentYear'] =currentYear;
+
+        speechOutput = `Your car is the ${currentYear} ${currentMake} ${currentModel}. You can ask me ` +
+            "your favorite color by saying, what's my favorite color?";
+        repromptText = "Thanks for adding a car! You can add another by saying add new car";
+    } else {
+        speechOutput = "I'm not sure what your mean. Please try again.";
+        repromptText = "I'm not sure what you mean. Please try again.";
+    }
+
+    callback(sessionAttributes,
+         buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function costCalculator(intent, session, callback) {
+    let currentMake;
+    let currentModel;
+    let currentYear;
+    const repromptText = null;
+    const sessionAttributes = session.attributes;
+    deivceId = session.attributes.deviceId;
+    consentToken = session.attributes.consentToken;
+
+    let shouldEndSession = false;
+    let speechOutput = '';
+    const location = intent.slots.location.value;
+    if (session.attributes) {
+        currentMake = session.attributes.currentMake;
+        currentModel = session.attributes.currentModel;
+        currentYear = session.attributes.currentYear;
+    }
+    sessionAttributes['location'] = location;
+
+    if (currentMake) {
+        speechOutput = `Your favorite color is ${currentMake}. Goodbye.`;
+        shouldEndSession = true;
+    } else {
+        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
+            ' is red';
+    }
+    session.attributes = sessionAttributes;
+    // Setting repromptText to null signifies that we do not want to reprompt the user.
+    // If the user does not respond or says something that is not understood, the session
+    // will end.
+    callback(sessionAttributes,
+         buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
+
+// --------------- Events -----------------------
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
+}
+
+/**
+ * Called when the user launches the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
+
+    // Dispatch to your skill's launch.
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
+
+    const intent = intentRequest.intent;
+    const intentName = intentRequest.intent.name;
+    // Dispatch to your skill's intent handlers
+    if (intentName === 'addCar') {
+        addCar(intent, session, callback);
+    } else if (intentName === 'costCalculator') {
+        costCalculator(intent, session, callback);
+    } else if (intentName === 'AMAZON.HelpIntent') {
+        getWelcomeResponse(callback);
+    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+        handleSessionEndRequest(callback);
+    } else {
+        throw new Error('Invalid intent');
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
+    // Add cleanup logic here
+}
+
+
+// --------------- Main handler -----------------------
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = (event, context, callback) => {
+    try {
+        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+        /*
+        if (event.session.application.applicationId !== 'amzn1.echo-sdk-ams.app.[unique-value-here]') {
+             callback('Invalid Application ID');
+        }
+        */
+
+        if (event.session.new) {
+            onSessionStarted({ requestId: event.request.requestId }, event.session);
+        }
+
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'IntentRequest') {
+            onIntent(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            callback();
+        }
+    } catch (err) {
+        callback(err);
+    }
+};
